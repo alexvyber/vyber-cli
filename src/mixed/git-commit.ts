8bb0e33a8dfbc8b4920c62c4ba9293fe401143b1
Alexey Sokolov <alexvyber@gmail.com>
2024-08-02 12:33:57 +0300

    Aug 2, 2024, 12:33:57 PM

--- src/mixed/git-commit.ts
+++ src/mixed/git-commit.ts
@@ -1,8 +1,11 @@
 import { exec } from "node:child_process"
 import { styled } from "../utils/styled"
+import { DateTime } from "luxon"
 
 async function run() {
-  exec(`git add -A && git commit -m "${new Date().toISOString()}" && git push`)
+  exec(
+    `git add -A && git commit -m "${DateTime.now().setLocale("en-US").toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}" && git push`
+  )
 
   console.log(styled.success("Auto commit complete!"))
 }
