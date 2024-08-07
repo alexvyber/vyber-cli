f26025b32210257793dabb5077587642b3e9fe75
Alexey Sokolov <alexvyber@gmail.com>
2024-08-07 14:39:29 +0300

    Aug 7, 2024, 2:39:29 PM

--- src/utils/get-config-content.ts
+++ src/utils/get-config-content.ts
@@ -1,6 +1,14 @@
+import { existsSync } from 'node:fs'
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
+import { homeDir } from './home-dir'
 
 export async function getConfigContent() {
+  const homeConfig = homeDir('.vyber.yaml')
+
+  if (existsSync(homeConfig)) {
+    return readFile(homeConfig, 'utf-8')
+  }
+
   return readFile(resolve(__dirname, '../config.yaml'), 'utf-8')
 }
