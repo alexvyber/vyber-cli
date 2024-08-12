c31441d9c10f004a8c7458d9ac15125033dcfbb6
Alexey Sokolov <alexvyber@gmail.com>
2024-08-12 19:54:37 +0300

    Aug 12, 2024, 7:54:37 PM

--- src/mixed/git-commit.ts
+++ src/mixed/git-commit.ts
@@ -1,20 +1,19 @@
 import { styled } from '../utils/styled'
 import { DateTime } from 'luxon'
-// import { execAsync } from '../utils/exec-async'
 import { execSync, ExecSyncOptionsWithBufferEncoding } from 'node:child_process'
 
 async function run(options?: ExecSyncOptionsWithBufferEncoding) {
+  const now = DateTime.now().setLocale('en-US').toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)
   const commands = [
     'git add -A',
-    `git commit -m "${DateTime.now().setLocale('en-US').toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}"`,
-    '[[ ! -z "$(git remote)" ]] && git push || exit 0',
+    `[[ ! -z "$(git status -s)" ]] && git commit -m "${now}" || exit 0`,
+    '[[ ! -z "$(git status -s)" ]] && [[ ! -z "$(git remote)" ]] && git push || exit 0',
   ]
 
   // it's better than command1 && ccommand2 && command3
   for (const command of commands) {
     try {
       execSync(command, options)
-      // execAsync(command)
     } catch (error) {
       if (error instanceof Error) {
         console.log(styled.error(error.message))
@@ -22,7 +21,7 @@ async function run(options?: ExecSyncOptionsWithBufferEncoding) {
     }
   }
 
-  console.log(styled.success('Auto commit complete!'))
+  console.log(styled.success(`Auto commit complete! ${options?.cwd ? options?.cwd : ''}`))
 }
 
 export default { run, title: 'Commit: auto git commit' }
