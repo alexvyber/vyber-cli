07e4d92603ed2c4d0a4ebd0a4486cabacec06c02
Alexey Sokolov <alexvyber@gmail.com>
2024-08-12 19:47:00 +0300

    Aug 12, 2024, 7:47:00 PM

--- src/mixed/auto-commit.ts
+++ src/mixed/auto-commit.ts
@@ -5,7 +5,7 @@ import gitCommit from './git-commit'
 async function run() {
   const { autocommit_repos } = await getConfig()
 
-  autocommit_repos.forEach((repo) => gitCommit.run({ cwd: homeDir(repo) }))
+  await Promise.allSettled([autocommit_repos.map((repo) => gitCommit.run({ cwd: homeDir(repo) }))])
 }
 
 export default { run, title: 'Auto Commit' }
