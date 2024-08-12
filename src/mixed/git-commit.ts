d2a106954b651be65a570ae1f91b8480e92a0f75
Alexey Sokolov <alexvyber@gmail.com>
2024-08-12 20:02:37 +0300

    Aug 12, 2024, 8:02:37 PM

--- src/mixed/git-commit.ts
+++ src/mixed/git-commit.ts
@@ -10,7 +10,6 @@ async function run(options?: ExecSyncOptionsWithBufferEncoding) {
     '[[ ! -z "$(git status -s)" ]] && [[ ! -z "$(git remote)" ]] && git push || exit 0',
   ]
 
-  // it's better than command1 && ccommand2 && command3
   for (const command of commands) {
     try {
       execSync(command, options)
